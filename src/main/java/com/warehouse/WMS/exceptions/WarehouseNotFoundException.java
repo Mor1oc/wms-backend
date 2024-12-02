1d84f53c6892c7d62a0b3a81d6c7b3224a0c267c
Vadim <vadimgolushko2004@gmail.com>
2024-12-02 13:45:26 +0300

    fourth commit

---
+++ src/main/java/com/warehouse/WMS/exceptions/WarehouseNotFoundException.java
@@ -0,0 +1,15 @@
+package com.warehouse.WMS.exceptions;
+
+import com.warehouse.WMS.component.services.CreateAllComponentsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WarehouseNotFoundException extends RuntimeException{
+
+    private static final Logger logger = LoggerFactory.getLogger(WarehouseNotFoundException.class);
+
+    public WarehouseNotFoundException() {
+        super(ErrorMessages.WAREHOUSE_NOT_FOUND.getMessage());
+        logger.error("Выброшено исключение {}", getClass());
+    }
+}
